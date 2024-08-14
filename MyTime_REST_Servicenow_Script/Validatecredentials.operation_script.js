10cc477307cfda06d9e4c4de9654f8804c50b886
Bruno <yupredtenteen@proton.me>
2024-08-14 22:26:33 +0530

    modify rest script in servicenow to receive and respond

--- MyTime_REST_Servicenow_Script/Validatecredentials.operation_script.js
+++ MyTime_REST_Servicenow_Script/Validatecredentials.operation_script.js
@@ -1,8 +1,28 @@
 (function process(/*RESTAPIRequest*/ request, /*RESTAPIResponse*/ response) {
 
-    // implement resource here
-	// var email = request.getHeader("email");
-	var queryParams = request.queryParams
-	gs.info("FROM REST EXPLORER: " + queryParams);
-	response.setBody({"response": "Hellow there. Responding from servicenow"});
+	//gets email from query param
+	var email= request.queryParams.sysparm_email;
+
+	//extracts the creds from string
+	var creds = (request.getHeader("Authorization")).split("Basic ")[1];
+	
+	//decodes base64 encrypted string
+	var decoded = GlideStringUtil.base64Decode(creds);
+	var decodedUsername = decoded.split(":")[0];
+	var decodedPassword = decoded.split(":")[1];
+	var gr = new GlideRecord("sys_user");
+	gr.addQuery("email",email);
+	gr.query();
+
+	while(gr.next()){
+		gs.info(GlideStringUtil.decodeAttributes(gr.user_password));
+		if(gr.user_password == decodedPassword){
+			gs.info("Password verified");
+		}
+		else{
+			gs.info("incorrect password");
+		}
+	}
+	// gs.info(decoded);
+	response.setBody({"response": "Hellow there. Responding from servicenow","creds": decoded});
 })(request, response);
