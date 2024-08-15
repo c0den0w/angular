435fbf107ff636e0ec1db8e4d920072f46c43eae
Bruno <yupredtenteen@proton.me>
2024-08-15 08:25:15 +0530

    basic auth succeeded

--- MyTime_REST_Servicenow_Script/Validatecredentials.operation_script.js
+++ MyTime_REST_Servicenow_Script/Validatecredentials.operation_script.js
@@ -1,28 +1,19 @@
-(function process(/*RESTAPIRequest*/ request, /*RESTAPIResponse*/ response) {
+(function process( /*RESTAPIRequest*/ request, /*RESTAPIResponse*/ response) {
 
-	//gets email from query param
-	var email= request.queryParams.sysparm_email;
 
-	//extracts the creds from string
-	var creds = (request.getHeader("Authorization")).split("Basic ")[1];
-	
-	//decodes base64 encrypted string
-	var decoded = GlideStringUtil.base64Decode(creds);
-	var decodedUsername = decoded.split(":")[0];
-	var decodedPassword = decoded.split(":")[1];
-	var gr = new GlideRecord("sys_user");
-	gr.addQuery("email",email);
-	gr.query();
+    //extracts the creds from string
+    var creds = (request.getHeader("Authorization")).split("Basic ")[1];
 
-	while(gr.next()){
-		gs.info(GlideStringUtil.decodeAttributes(gr.user_password));
-		if(gr.user_password == decodedPassword){
-			gs.info("Password verified");
-		}
-		else{
-			gs.info("incorrect password");
-		}
-	}
-	// gs.info(decoded);
-	response.setBody({"response": "Hellow there. Responding from servicenow","creds": decoded});
+    //decodes base64 encrypted string
+    var decoded = GlideStringUtil.base64Decode(creds);
+    var decodedUsername = decoded.split(":")[0];
+    var decodedPassword = decoded.split(":")[1];
+
+    if (GlideUser.authenticateUser(decodedUsername, decodedPassword)) {
+        gs.info("Password verified");
+        response.setStatus(200);
+        response.setBody({
+            "message": "User Authenticated!!!",
+        });
+    }
 })(request, response);
