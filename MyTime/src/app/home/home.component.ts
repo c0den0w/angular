1c6e98f6bb50db9eb921dd88035c1add95136fc2
Bruno <yupredtenteen@proton.me>
2024-08-21 23:10:02 +0530

    add cards to pending actions

--- MyTime/src/app/home/home.component.ts
+++ MyTime/src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { PendingActionsComponent } from './pending-actions/pending-actions.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [PendingActionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
