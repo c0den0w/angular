2428bfde4a2784ac195c68e7b5daa97e93363b33
Bruno <yupredtenteen@proton.me>
2024-09-03 22:32:47 +0530

    modify layout

---
+++ QPaper/src/app/qpaper/layout-options/layout-options.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout-options.component.html',
+  styleUrl: './layout-options.component.scss'
+})
+export class LayoutOptionsComponent {
+
+}
--- QPaper/src/app/qpaper/qpaper.component.ts
+++ QPaper/src/app/qpaper/qpaper.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { WorkareaComponent } from './workarea/workarea.component';
 import { WidgetsComponent } from './widgets/widgets.component';
+import { LayoutOptionsComponent } from './layout-options/layout-options.component';
 
 @Component({
   selector: 'app-qpaper',
   standalone: true,
-  imports: [WidgetsComponent, WorkareaComponent],
+  imports: [WidgetsComponent, WorkareaComponent, LayoutOptionsComponent],
   templateUrl: './qpaper.component.html',
   styleUrl: './qpaper.component.scss'
 })
 export class QpaperComponent {
-  
+
 }
