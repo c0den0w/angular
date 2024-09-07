712234a0e1a97568636e689946dbeef9f23c7a81
Bruno <yupredtenteen@proton.me>
2024-09-07 23:16:10 +0530

    putting things in place

--- QPaper/src/app/qpaper/widgets/widgets.component.ts
+++ QPaper/src/app/qpaper/widgets/widgets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { SearchWidgetsComponent } from './search-widgets/search-widgets.component';
+import { GroupComponent } from './group/group.component';
 @Component({
   selector: 'app-widgets',
   standalone: true,
-  imports: [],
+  imports: [SearchWidgetsComponent, GroupComponent],
   templateUrl: './widgets.component.html',
   styleUrl: './widgets.component.scss'
 })
