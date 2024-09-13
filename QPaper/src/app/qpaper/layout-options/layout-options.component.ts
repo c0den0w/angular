c1ed17592b90d54bd7aaccbe14620a88336c9c63
Bruno <yupredtenteen@proton.me>
2024-09-13 23:45:12 +0530

    commit dropdown

--- QPaper/src/app/qpaper/layout-options/layout-options.component.ts
+++ QPaper/src/app/qpaper/layout-options/layout-options.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-layout-options',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './layout-options.component.html',
   styleUrl: './layout-options.component.scss'
 })
 export class LayoutOptionsComponent {
-
+  showDropdown:boolean = false;
+  dropdownTitle:string = "Page size";
+  toggleDropdown(){
+    this.showDropdown = !this.showDropdown;
+  }
+  selectpageSize(e: any){
+    this.dropdownTitle = e.target.innerText;
+    this.showDropdown = !this.showDropdown;
+  }
 }
