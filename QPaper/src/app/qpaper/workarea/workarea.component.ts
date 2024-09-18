af2ead75f92ff3a14f50ffe3cb49a2945e1ca8ce
Bruno <yupredtenteen@proton.me>
2024-09-18 21:07:51 +0530

    add code to scale up and down A4 size page

--- QPaper/src/app/qpaper/workarea/workarea.component.ts
+++ QPaper/src/app/qpaper/workarea/workarea.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './workarea.component.scss'
 })
 export class WorkareaComponent {
+  pageScaleFactor: number = 5;
+  zoomin(e: any){
+    console.log(e.wheelDelta);
+    if(e.wheelDelta < 0){
+      document.getElementById("paper")!.style.width = `calc((${this.pageScaleFactor} + ${this.pageScaleFactor} * 0.01) * 210px)`;
+      document.getElementById("paper")!.style.height = `calc((${this.pageScaleFactor} - ${this.pageScaleFactor} * 0.01) * 297px)`;
 
+      this.pageScaleFactor = this.pageScaleFactor + this.pageScaleFactor * 0.01;
+    }
+    if(e.wheelDelta > 0){
+      document.getElementById("paper")!.style.width = `calc((${this.pageScaleFactor} - ${this.pageScaleFactor} * 0.01) * 210px)`;
+      document.getElementById("paper")!.style.height = `calc((${this.pageScaleFactor} - ${this.pageScaleFactor} * 0.01) * 297px)`;
+
+      this.pageScaleFactor = this.pageScaleFactor - this.pageScaleFactor * 0.01;
+    }
+  }
 }
